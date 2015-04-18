5ae5e27d40e4e094e96f68d82027d7fbf065f0e5
Zach Deibert <zachdeibert@gmail.com>
2015-04-18 16:18:08 -0500

    Added potion effect transfers during Zombie Apocalypses and added projectile handlers for the Zombie Apocalypse hit detection

--- src/main/java/com/gitlab/zachdeibert/WarPlugin/ZombieApocalypseMode.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/ZombieApocalypseMode.java
@@ -2,22 +2,29 @@ package com.gitlab.zachdeibert.WarPlugin;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
 
 public class ZombieApocalypseMode implements Listener {
     @EventHandler
     public void onEntityDamageByEntity(final EntityDamageByEntityEvent event) {
-        if ( event.getDamager() instanceof Player && event.getEntity() instanceof Zombie ) {
+        final Entity damager = event.getDamager();
+        if ( (damager instanceof Player || (damager instanceof Projectile && ((Projectile) damager).getShooter() instanceof Player)) && event.getEntity() instanceof Zombie ) {
             final Zombie parent = (Zombie) event.getEntity();
             if ( parent.getHealth() > event.getDamage() ) {
                 final Location loc = parent.getLocation();
                 final World world = loc.getWorld();
                 final Zombie child = (Zombie) world.spawnEntity(loc, EntityType.ZOMBIE);
+                for ( final PotionEffect potion : parent.getActivePotionEffects() ) {
+                    child.addPotionEffect(potion);
+                }
                 child.setHealth(parent.getHealth());
                 child.setTarget(parent.getTarget());
                 child.setVelocity(parent.getVelocity());
