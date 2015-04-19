7be75ed65d37cfe5b141419f9c565d87892b7a2f
Zach Deibert <zachdeibert@gmail.com>
2015-04-19 16:38:55 -0500

    Fixed NullPointerException

--- src/main/java/com/gitlab/zachdeibert/WarPlugin/WarEnder.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/WarEnder.java
@@ -35,10 +35,12 @@ public class WarEnder implements Listener {
                     }
                 }
             }
-            Util.broadcastMessage("§2The war has ended!§r");
-            Util.broadcastMessage(String.format("§2%s has won the war!§r", winner.getName()));
-            if ( onWin != null ) {
-                onWin.run();
+            if ( winner != null ) {
+                Util.broadcastMessage("§2The war has ended!§r");
+                Util.broadcastMessage(String.format("§2%s has won the war!§r", winner.getName()));
+                if ( onWin != null ) {
+                    onWin.run();
+                }
             }
         }
     }
