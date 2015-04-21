6e4c599ebb262db75d91d659738a7770fe7946b7
Zach Deibert <zachdeibert@gmail.com>
2015-04-21 16:40:54 -0500

    Added death inventory serialization

--- src/main/java/com/gitlab/zachdeibert/WarPlugin/DeathHandler.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/DeathHandler.java
@@ -1,11 +1,15 @@
 package com.gitlab.zachdeibert.WarPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,8 +20,56 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class DeathHandler implements Listener {
-    private final Map<Player, List<ItemStack>> inventories;
+    private final Map<String, List<ItemStack>> inventories;
     private double dropChance;
+    
+    private void serializeMap() {
+        try {
+            final File worldDir = Bukkit.getWorlds().get(0).getWorldFolder();
+            final File serialFile = new File(worldDir, getClass().getName().concat(".Inventories"));
+            final FileConfiguration serial = new YamlConfiguration();
+            for ( final String name : inventories.keySet() ) {
+                serial.set(name, inventories.get(name));
+            }
+            serial.save(serialFile);
+        } catch ( final Exception ex ) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private Map<String, List<ItemStack>> deserializeMap() {
+        try {
+            final File worldDir = Bukkit.getWorlds().get(0).getWorldFolder();
+            final File serialFile = new File(worldDir, getClass().getName().concat(".Inventories"));
+            if ( serialFile.exists() ) {
+                final FileConfiguration serial = new YamlConfiguration();
+                serial.load(serialFile);
+                final Map<String, Object> deserialized = serial.getValues(false);
+                final Map<String, List<ItemStack>> map = new HashMap<String, List<ItemStack>>();
+                for ( final String name : deserialized.keySet() ) {
+                    @SuppressWarnings("unchecked")
+                    final List<ItemStack> list = (List<ItemStack>) deserialized.get(name);
+                    map.put(name, list);
+                }
+                return map;
+            }
+        } catch ( final Exception ex ) {
+            ex.printStackTrace();
+        }
+        return new HashMap<String, List<ItemStack>>();
+    }
+    
+    private List<ItemStack> getList(final String name) {
+        if ( inventories.containsKey(name) ) {
+            final List<ItemStack> list = inventories.get(name);
+            if ( list != null ) {
+                return list;
+            }
+        }
+        final List<ItemStack> list = new ArrayList<ItemStack>();
+        inventories.put(name, list);
+        return list;
+    }
     
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerDeathMessageGeneration(final PlayerDeathEvent event) {
@@ -36,14 +88,13 @@ public class DeathHandler implements Listener {
     @EventHandler
     public void onPlayerRespawn(final PlayerRespawnEvent event) {
         final Player player = event.getPlayer();
-        if ( inventories.containsKey(player) ) {
-            final List<ItemStack> items = inventories.get(player);
-            inventories.remove(player);
-            final PlayerInventory inv = player.getInventory();
-            for ( final ItemStack stack : items ) {
-                inv.addItem(stack);
-            }
+        final PlayerInventory inv = player.getInventory();
+        final List<ItemStack> items = getList(player.getName());
+        while ( items.size() > 0 ) {
+            inv.addItem(items.get(0));
+            items.remove(0);
         }
+        serializeMap();
     }
     
     @EventHandler(ignoreCancelled = true)
@@ -51,18 +102,18 @@ public class DeathHandler implements Listener {
         final Player player = event.getEntity();
         final List<ItemStack> drops = event.getDrops();
         event.setKeepLevel(true);
-        final List<ItemStack> removedDrops = new ArrayList<ItemStack>();
+        final List<ItemStack> removedDrops = getList(player.getName());
         final int numberOfDrops = (int) (drops.size() * dropChance);
         drops.sort(new RandomComparator<ItemStack>(dropChance));
         for ( int i = 0; i < numberOfDrops; i++ ) {
             removedDrops.add(drops.get(0));
             drops.remove(0);
         }
-        inventories.put(player, removedDrops);
+        serializeMap();
     }
     
     public void setDropChance(final double chance) {
-        dropChance = chance;
+        dropChance = 1.0 - chance;
     }
     
     public void load(final FileConfiguration config, final String prefix) {
@@ -70,8 +121,8 @@ public class DeathHandler implements Listener {
         setDropChance(config.getDouble(prefix.concat(".Drop.Chance")));
     }
     
-    public DeathHandler() {
-        inventories = new HashMap<Player, List<ItemStack>>();
+    public DeathHandler() throws IOException {
+        inventories = deserializeMap();
         dropChance = 0.1;
     }
 }
