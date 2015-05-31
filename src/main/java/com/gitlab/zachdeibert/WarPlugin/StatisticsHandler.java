24085860dc5516e95ba0ecb58df0f338403b1ffa
Zach Deibert <zachdeibert@gmail.com>
2015-05-31 16:35:47 -0500

    Reverted to old version of statistics handler (server admins can set up scoreboards themselves if they want to use them)

--- src/main/java/com/gitlab/zachdeibert/WarPlugin/StatisticsHandler.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/StatisticsHandler.java
@@ -1,71 +1,43 @@
 package com.gitlab.zachdeibert.WarPlugin;
 
+import java.io.File;
 import java.io.IOException;
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 public class StatisticsHandler implements Listener {
-    private final ScoreboardManager scoreboardManger;
-    private final Scoreboard        scoreboard;
-    private final Objective         deaths;
+    private File databaseFile;
+    private YamlConfiguration database;
+    
+    public void load(final File dir) throws IOException {
+        try {
+            if ( !dir.exists() ) {
+                dir.mkdirs();
+            }
+            databaseFile = new File(dir, "statistics.yml");
+            if ( !databaseFile.exists() ) {
+                databaseFile.createNewFile();
+            }
+            database = new YamlConfiguration();
+            database.load(databaseFile);
+        } catch ( final InvalidConfigurationException ex ) {
+            throw new IOException(ex);
+        }
+    }
     
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerDeath(final PlayerDeathEvent event) throws IOException {
         final Player player = event.getEntity();
         final String name = player.getName();
-        final int deaths = this.deaths.getScore(name).getScore();
+        final int deaths = database.getInt(name, 0) + 1;
+        database.set(name, deaths);
+        database.save(databaseFile);
         final String deathMessage = String.format("%s [Death #%d]", event.getDeathMessage(), deaths);
         event.setDeathMessage(deathMessage);
-    }
-    
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerLogin(final PlayerLoginEvent event) throws IOException {
-        final Player player = event.getPlayer();
-        final String name = player.getName();
-        final Score score = deaths.getScore(name);
-        if ( score == null || score.getScore() == 0 ) {
-            score.setScore(1);
-            score.setScore(0);
-        }
-    }
-    
-    public StatisticsHandler() throws IOException {
-        scoreboardManger = Bukkit.getScoreboardManager();
-        scoreboard = scoreboardManger.getMainScoreboard();
-        Objective deaths = null;
-        Objective kills = null;
-        for ( final Objective obj : scoreboard.getObjectives() ) {
-            final String name = obj.getName();
-            if ( name.equals("Deaths") ) {
-                deaths = obj;
-                if ( kills != null ) {
-                    break;
-                }
-            } else if ( name.equals("Kills") ) {
-                kills = obj;
-                if ( deaths != null ) {
-                    break;
-                }
-            }
-        }
-        if ( deaths == null ) {
-            deaths = scoreboard.registerNewObjective("Deaths", "dummy");
-        }
-        if ( kills == null ) {
-            kills = scoreboard.registerNewObjective("Kills", "totalKills");
-        }
-        deaths.setDisplaySlot(DisplaySlot.SIDEBAR);
-        kills.setDisplaySlot(DisplaySlot.PLAYER_LIST);
-        this.deaths = deaths;
     }
 }
--- src/main/java/com/gitlab/zachdeibert/WarPlugin/WarPlugin.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/WarPlugin.java
@@ -145,6 +145,7 @@ public class WarPlugin extends JavaPlugin {
             ender = new WarEnder(server);
             ender.load(config, "War.Winning", dataFolder);
             final StatisticsHandler stats = new StatisticsHandler();
+            stats.load(dataFolder);
             SpawnEggHandler.setup(server, config, "Monsters.Eggs");
             ExpHandler.setup(server, config, "Exp");
             final DeathHandler deaths = new DeathHandler();
