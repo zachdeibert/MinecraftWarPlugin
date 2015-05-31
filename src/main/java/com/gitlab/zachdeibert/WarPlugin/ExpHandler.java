b763ce0c03a9c61ae6ab8581e654cf74c2f643e2
Zach Deibert <zachdeibert@gmail.com>
2015-05-31 16:53:45 -0500

    Added count variable to exp handler

--- src/main/java/com/gitlab/zachdeibert/WarPlugin/ExpHandler.java
+++ src/main/java/com/gitlab/zachdeibert/WarPlugin/ExpHandler.java
@@ -8,14 +8,15 @@ import org.bukkit.inventory.ShapedRecipe;
 
 public class ExpHandler extends ShapedRecipe {
     public static void setup(final Server server, final FileConfiguration config, final String prefix) {
-        config.addDefault(prefix.concat("..Bottles.Enabled"), true);
+        config.addDefault(prefix.concat(".Bottles.Enabled"), true);
+        config.addDefault(prefix.concat(".Bottles.Count"), 1);
         if ( config.getBoolean(prefix.concat(".Bottles.Enabled")) ) {
-            server.addRecipe(new ExpHandler());
+            server.addRecipe(new ExpHandler(config.getInt(prefix.concat(".Bottles.Count"))));
         }
     }
     
-    private ExpHandler() {
-        super(new ItemStack(Material.EXP_BOTTLE));
+    private ExpHandler(final int count) {
+        super(new ItemStack(Material.EXP_BOTTLE, count));
         shape("sps", "gdg", "dgd");
         setIngredient('d', Material.DIAMOND);
         setIngredient('g', Material.GLASS);
